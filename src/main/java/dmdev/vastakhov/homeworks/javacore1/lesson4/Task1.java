1c5ed9094c51ed99499f99a99bb6084eac53ac93
Астахов Вадим Андреевич <vaaastakhov@beeline.ru>
2022-09-20 10:22:39 +0300

    second commit

--- src/main/java/dmdev/vastakhov/homeworks/javacore1/lesson4/Task1.java
+++ src/main/java/dmdev/vastakhov/homeworks/javacore1/lesson4/Task1.java
@@ -3,5 +3,6 @@ package dmdev.vastakhov.homeworks.javacore1.lesson4;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Go!");
+        System.out.println("kuku");
     }
 }
